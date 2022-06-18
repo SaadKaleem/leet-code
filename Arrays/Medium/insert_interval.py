626e3eb111a31aca52bbca93bba21d60097fd861
Saad Kaleem <mk1703916@qu.edu.qa>
2022-06-19 01:14:17 +0300

    TODO InsertInterval

---
+++ Arrays/Medium/insert_interval.py
@@ -0,0 +1,17 @@
+"""
+xx/06/2022 - https://leetcode.com/problems/insert-interval/
+
+~~
+"""
+
+from typing import List
+
+"""
+
+For list of n items...
+Time complexity: 
+Space complexity: 
+"""
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        #TODO
